638d83902c5e8092124959e0aa51b574823fd5fc
ineedjobtostartwork <ineedjobtostartwork@gmail.com>
2024-01-01 18:54:20 +0100

    refactor(docs): cjs => esm next.config.js

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,9 +1,11 @@
-const withNextra = require("nextra")({
+import nextraDef from "nextra";
+
+const withNextra = nextraDef({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx"
 });
 
-module.exports = withNextra();
+export default withNextra();
 // module.exports = withNextra({
 //   i18n: {
 //     locales: ['en', 'pl', 'es','fr','de','it','ru','zh','tr','ja','hi'],
